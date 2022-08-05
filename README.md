9588d3897dfc2077f43c2639babda4a9230e9ff2
Fabrice Gallet <fbgallet@gmail.com>
2022-08-06 01:57:51 +0200

    Keep last color mode

--- README.md
+++ README.md
@@ -5,17 +5,21 @@
 ![image](https://user-images.githubusercontent.com/74436347/182989132-886f244e-a37b-40fa-8010-04bdee184e16.png)
 
 ## Instructions
-Select some part of your text inside a block, add Highight with native Roam hotkeys (`Ctrl + h` on Windows, `Cmd + h` on iOS), then **press a letter corresponding to the first letter of the color (without pressing Ctrl-Cmd)**, e.g. 'g' for green. If you press `Shift` when pressing the color first letter, the color will be more vivid and the text color in the highlight will be white instead of black.
+
+Select some part of your text inside a block, add Highight with native Roam hotkeys (`Ctrl + h` on Windows, `Cmd + h` on iOS), then **press a letter corresponding to the first letter of the color (**without pressing Ctrl-Cmd anymore**)**, e.g. 'g' for green. If you press `Shift` when pressing the color first letter, the color will be more vivid and the text color in the highlight will be white instead of black.
 
 The extension will insert a tag like `#c:green` (or #c:GREEN) before highlight or bold markdown syntax, the result will be e.g.: `#c:green**my green text**`. Of course, these tags are only visible when you edit the block. _Credit to @CatoMinor3 for the great CSS trick allowing to customize highlighted and bold texts in Roam!_
 
+By default, you must choose a color each time, but you enable `Keep last color` option, the last applied color will be automaticaly applied when pressing `Ctrl/Cmd + b/h` next times. When tag color has just been inserted, you can change it by pressing the corresponding color letter or reset to default bold/highlight by pression `Backspace`.
+
 After a color choice, the **cursor** will be placed just next the colored text, right next the markdown characters to be exact, so you can write something else after. In option (panel settings above), you can choose to keep Roam's default behavior: highlighted or bolded text will be selected.
 
-⚠️ If no text is initialy selected (you want to write something new), the cursor will be placed inside the markups, so you can immediatly write. But since the color trigger letters could conflict with the first letter you might write in a native hightlight or bold mode, **you need to enable "color mode" by pressing again "Ctrl/Cmd" in this case**.
+⚠️ If no text is initialy selected (you want to write something new in bold or highlight mode), the cursor will be placed inside the markups, so you can immediatly write. But since the color trigger letters could conflict with the first letter you might write, **you need to enable "color mode" by pressing again "Ctrl/Cmd" in this case**. Then, you can choose the color.
 
 Current default set of colors (_initial letter ⇒ tag_)
+
 - b ⇒ #c:blue
-- B ⇒ #c:BLUE 
+- B ⇒ #c:BLUE
 - f ⇒ #c:fuchsia
 - F ⇒ #c:FUCHSIA
 - g ⇒ #c:green
@@ -30,21 +34,23 @@ Current default set of colors (_initial letter ⇒ tag_)
 - y ⇒ #c:yellow
 - w ⇒ #c:black
 
-Settings allow you to use your own set of color tags and corresponding trigger letters. If you enter something in the setting fields, only your tags will be available. If you remove your settings, default tags will be applied.
-
 Don't overuse colors ! Don't forget that the extension inserts tags in your content, which reduces its portability (see below the commands to quickly remove tags).
 
 ## Bonus commands
-There is three commands in the Command Palette (Ctrl-Cmd + p) to export more easily you data, since colors tags are readable only in Roam with the current extension:
-- remove color tags from current block (`Atl + h` do the same)
-- Remove color tags & bold/hightlighs/italics markups from current block (^^, \*\*, \_\_)
-- Remove all color tags from current page (the whole page is processed, even if you are zooming on a part only)
+
+There is two commands in the Command Palette (Ctrl-Cmd + p) for removing colors tags (it can help to export more easily you data, since colors tags are readable only in Roam with the current extension):
+
+- Remove color tags from current block (`Atl + h` do the same)
+- Remove all color tags from current page view (current zoom, not the whole page, but collapsed bullets included)
+  In the setting panel, you can choose if you prefer to remove only tags (default) or tags and format Markdown syntax (^^, \*\*, \_\_)
 
 ## Future developments
+
 - This extension is currently keyboard only, I could add some visual UI.
 - Color whole block and/or its children.
 - Colored underlining, colored borders.
 - Better colors customization.
 
 ---
+
 For any question or suggestion, DM me on Twitter: [@fbgallet](https://twitter.com/fbgallet) or Roam Slack.
--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ var colorTags = [];
 const colorKeysDefault = ["b", "B", "f", "F", "g", "G", "o", "O",
                           "s", "S", "r", "R", "t", "y", "w"];
 var colorKeys = [];
-var cursorAfter;
+var cursorAfter, removeOption, keepColor;
+var lastColorH = '';
+var lastColorT = '';
 
 function keyHighlight(e) {
   if (hFlag) {
@@ -18,38 +20,64 @@ function keyHighlight(e) {
     if (!needConfirmKey) {
       let color = checkColorKeys(e.key);
       if (color!='') {
-        addColor(color);
+        if (keepColor && lastColorH != '') {
+          window.roamAlphaAPI.data.undo();
+          lastColorH = color;
+        }
+        setTimeout(function() {addColor(color, 'h');},50);
         e.preventDefault();
       }
     }
     if (e.ctrlKey) needConfirmKey=false;
+    if (e.key == 'Backspace') {
+      hFlag = false;
+      window.roamAlphaAPI.data.undo();
+      lastColorH = '';
+      e.preventDefault();
+    }
   }
   if (tFlag) {
     if (!e.shiftKey && !e.ctrlKey) tFlag = false;
     if (!needConfirmKey) {
       let color = checkColorKeys(e.key);
       if (color!='') {
-        addColor(color);
+        if (keepColor && lastColorT != '') {
+          window.roamAlphaAPI.data.undo();
+          lastColorT = color;
+        }
+        setTimeout(function() {addColor(color, 't');},50);
         e.preventDefault();
       }
     }
     if (e.ctrlKey) needConfirmKey=false;
+    if (e.key == 'Backspace') {
+      tFlag = false;
+      window.roamAlphaAPI.data.undo();
+      lastColorT = '';
+      e.preventDefault();
+    }
   }
   if (tFlag !=true && hFlag!=true) {
     if (e.ctrlKey && e.key == "h") {
+      if (keepColor && lastColorH!='') {
+        setTimeout(function() {addColor(lastColorH, 'h');},100);
+      }
       hFlag = true;
       if (hasSelection()) needConfirmKey=false;
       else needConfirmKey=true;
       return;
     }
     if (e.ctrlKey && e.key == "b") {
+      if (keepColor && lastColorT!='') {
+        setTimeout(function() {addColor(lastColorT, 't');},100); 
+      }
       tFlag = true;
       if (hasSelection()) needConfirmKey=false;
       else needConfirmKey=true;
     }
     if ((e.altKey && e.key == "h")) {
       //hFlag = true;
-      removeHighlightsFromBlock();
+      removeHighlightsFromBlock(null,removeOption);
       setCursorPosition(document.activeElement)
     }
   }
@@ -67,14 +95,12 @@ function checkColorKeys(key) {
     }
     return '';
 }
-/*
-function countDown(c) {
-  setTimeout(( ) ⇒ {
 
-  })
-}*/
-
-function addColor(color) {
+function addColor(color, flag) {
+    if (keepColor) {
+      if (flag=='h' && lastColorH=='') lastColorH=color;
+      if (flag=='t' && lastColorT=='') lastColorT=color;
+    }
     let tagLength=color.length;
     let uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
     let content = getBlockContent(uid);
@@ -100,27 +126,28 @@ function setCursorPosition(input,start=0,end=0,length=0) {
               input.selectionEnd = end+length;
           }
         }
-      },50);
+      },40);
   return;
 }
 
-function removeHighlightsFromBlock(uid, removeH=false,removeB=false,removeI=false) {
-  //let uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+function removeHighlightsFromBlock(uid=null, removeMarkups=false) {
+  if (uid==null) uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
   let content = getBlockContent(uid);  
-  content = removeFromContent(content, removeH, removeB, removeI);
+  content = removeFromContent(content, removeMarkups);
   setTimeout(function() {
     window.roamAlphaAPI
               .updateBlock({'block': 
                   {'uid': uid,
                   'string': content}});
   },10)
-  
 }
 
-function removeFromContent(content, removeH=false,removeB=false,removeI=false) {
-  if (removeH) content = content.replaceAll("^^","");
-  if (removeB) content = content.replaceAll("**","");
-  if (removeI) content = content.replaceAll("__","");
+function removeFromContent(content, removeMarkups=false) {
+  if (removeMarkups) {
+    content = content.replaceAll("^^","");
+    content = content.replaceAll("**","");
+    content = content.replaceAll("__","");
+  }
   for(let i=0;i<colorTags.length;i++) {
     content = content.replaceAll(colorTags[i],"");  
   }
@@ -131,32 +158,27 @@ function recursiveCleaning(branch) {
   for (let i=0;i<branch.length;i++) {
     let nodeUid = branch[i].uid; 
     let nodeContent = branch[i].string;
-    let newContent = removeFromContent(nodeContent);
-    console.log(newContent)
-    console.log(nodeUid);
+    let newContent = removeFromContent(nodeContent,removeOption);
     if (nodeContent.length != newContent.length) {
-      //console.log(nodeContent);
-
+      console.log(nodeContent);
       setTimeout(function() {
         window.roamAlphaAPI
                   .updateBlock({'block': 
                       {'uid': nodeUid,
                       'string': newContent}})
-      }, 50);
+      }, 20);
     }
     if (branch[i].children) recursiveCleaning(branch[i].children);
   }
 }
 
-function getPageTreeByBlockUid(bUid) {
+function getPageViewTreeByBlockUid(bUid) {
   let tree = window.roamAlphaAPI.q(`
-    [:find (pull ?page 
+    [:find (pull ?pageview 
       [ :block/string :block/uid :block/children
-        {:block/page ...}
         {:block/children ...}])
-          :where [?page :block/uid "${bUid}"]]`)[0][0];
-  if (tree.page) return tree.page.children;
-  else return tree.children;
+          :where [?pageview :block/uid "${bUid}"]]`)[0][0];
+    return tree;
 }
 
 function getBlockContent(uid) {
@@ -173,13 +195,13 @@ function getBlockContent(uid) {
 const panelConfig = {
     tabTitle: "Color Highlighter",
     settings: [
-        {id:     "color-tags",
+ /*       {id:     "color-tags",
         name:   "Color tags",
         description: "Customized list of color tags, separated with a comma",
         action: {type:        "input",
                 placeholder: "#c:red, #c:green",
                 onChange:    (evt) => { 
-//                  colorTags = evt.target.value;
+                  colorTags = evt.target.value;
                  }}},
         {id:     "color-keys",
          name:   "Color keys",
@@ -187,14 +209,28 @@ const panelConfig = {
          action: {type:     "input",
                   onChange: (evt) => { 
         //         colorKeys = evt.taget.value;
-         }}},
+         }}},*/
+         {id:         "keep-color",
+         name:        "Keep last color",
+         description: "Apply automaticaly last color when highlighting or bolding text (backspace to reset)",
+         action:      {type:     "switch",
+                       onChange: (evt) => { 
+                         keepColor = !keepColor;
+                       }}},
         {id:          "cursor-position",
         name:        "Cursor just after highlighted/colored text",
         description: "If disabled, text highlighted/colored will be selected (native Roam behavior)",
         action:      {type:     "switch",
                       onChange: (evt) => { 
                         cursorAfter = !cursorAfter;
-                      }}}
+                      }}},
+        {id:          "remove-option",
+        name:        "Remove Mardown format charactere when removing color tags",
+        description: "If enable, remove ^^,**,__ when removing tags from block or page view (command palette)",
+        action:      {type:     "switch",
+                      onChange: (evt) => { 
+                        removeOption = !removeOption;
+                      }}},
     ]
   }; 
 
@@ -203,38 +239,44 @@ export default {
         extensionAPI.settings.panel.create(panelConfig);
         window.roamAlphaAPI.ui.commandPalette.addCommand({
             label: "Remove color tags from current block (Color Highlighter extension)",
-            callback: () => {
-                let uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
-                removeHighlightsFromBlock(uid);
+            callback: async () => {
+                let block = window.roamAlphaAPI.ui.getFocusedBlock();              
+                removeHighlightsFromBlock(block["block-uid"], removeOption);
+                setTimeout(function(){window.roamAlphaAPI.ui.setBlockFocusAndSelection({location: block})},250);
             }
         })
         window.roamAlphaAPI.ui.commandPalette.addCommand({
-            label: "Remove color tags & bold/hightlighs/italics markups from current block (Color Highlighter extension)",
-            callback: () => {
-                let uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
-                removeHighlightsFromBlock(uid, true,true,true);
-            }
-        })
-        window.roamAlphaAPI.ui.commandPalette.addCommand({
-          label: "Remove all color tags from current page (Color Highlighter extension)",
+          label: "Remove all color tags from current page zoom view (Color Highlighter extension)",
           callback: async () => {        
             let uid = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();             
             console.log(uid);
-            const pageTree = getPageTreeByBlockUid(uid);
-            recursiveCleaning(pageTree);
+            const tree = getPageViewTreeByBlockUid(uid);
+            console.log(tree);
+            if (typeof(tree.string) != 'undefined') removeHighlightsFromBlock(tree.uid, removeOption);
+            recursiveCleaning(tree.children);
           }
       }) 
-        if (extensionAPI.settings.get("color-tags") == null)
+    //    if (extensionAPI.settings.get("color-tags") == null)
             colorTags = colorTagsDefault;
-        else colorTags = extensionAPI.settings.get("color-tags").replace(' ','').split(",");
-        if (extensionAPI.settings.get("color-keys") == null)
+    //    else colorTags = extensionAPI.settings.get("color-tags").replace(' ','').split(",");
+    //    if (extensionAPI.settings.get("color-keys") == null)
             colorKeys = colorKeysDefault;
-        else colorTags = extensionAPI.settings.get("color-keys").replace(' ','').split(",");
+    //    else colorTags = extensionAPI.settings.get("color-keys").replace(' ','').split(",");
+        if (extensionAPI.settings.get("keep-color") == null) {
+          extensionAPI.settings.set("keep-color", false);
+          keepColor = false;
+        }
+        else keepColor = extensionAPI.settings.get("keep-color");
         if (extensionAPI.settings.get("cursor-position") == null) {
             extensionAPI.settings.set("cursor-position", true);
             cursorAfter = true;
         }
         else cursorAfter = extensionAPI.settings.get("cursor-position");
+        if (extensionAPI.settings.get("remove-option") == null) {
+          extensionAPI.settings.set("remove-option", false);
+          removeOption = false;
+      }
+      else removeOption = extensionAPI.settings.get("remove-option");
 
         window.addEventListener("keydown", keyHighlight);
         hFlag = false;   
